6992d4a0b1087657cae0ba2576cbab1f5e1276e0
Michael Cooper <mc454916@ohio.edu>
2021-11-04 22:34:38 -0400

    Refactor Default Class Name

--- src/main.cpp
+++ src/main.cpp
@@ -3,50 +3,56 @@
 #ifndef WX_PRECOMP
     #include <wx/wx.h>
 #endif
-class MyApp: public wxApp
+class TimeLogger: public wxApp
 {
 public:
     virtual bool OnInit();
 };
-class MyFrame: public wxFrame
+class TLFrame: public wxFrame
 {
 public:
-    MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
+    TLFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
 private:
     void OnHello(wxCommandEvent& event);
     void OnExit(wxCommandEvent& event);
     void OnAbout(wxCommandEvent& event);
     wxDECLARE_EVENT_TABLE();
 };
-wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
-    EVT_MENU(wxID_EXIT,  MyFrame::OnExit)
-    EVT_MENU(wxID_ABOUT, MyFrame::OnAbout)
+
+wxBEGIN_EVENT_TABLE(TLFrame, wxFrame)
+    EVT_MENU(wxID_EXIT,  TLFrame::OnExit)
+    EVT_MENU(wxID_ABOUT, TLFrame::OnAbout)
 wxEND_EVENT_TABLE()
-wxIMPLEMENT_APP(MyApp);
-bool MyApp::OnInit()
+wxIMPLEMENT_APP(TimeLogger);
+
+// Load up the user's chosen default frame
+bool TimeLogger::OnInit()
 {
-    MyFrame *frame = new MyFrame( "Hello World", wxPoint(50, 50), wxSize(450, 340) );
+    TLFrame *frame = new TLFrame( "Time Logger", wxPoint(50, 50), wxSize(750, 500) );
     frame->Show( true );
     return true;
 }
-MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
+TLFrame::TLFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
         : wxFrame(NULL, wxID_ANY, title, pos, size)
 {
     wxMenu *menuFile = new wxMenu;
+    // Save / Backup / Export
+    // menuFile->AppendSeparator();
+    menuFile->Append(wxID_EXIT, "&Quit\tCtrl-Q");
     wxMenu *menuHelp = new wxMenu;
     menuHelp->Append(wxID_ABOUT);
     wxMenuBar *menuBar = new wxMenuBar;
     menuBar->Append( menuFile, "&File" );
     menuBar->Append( menuHelp, "&Help" );
     SetMenuBar( menuBar );
-    CreateStatusBar();
+    // CreateStatusBar();
     SetStatusText( "Welcome to wxWidgets!" );
 }
-void MyFrame::OnExit(wxCommandEvent& event)
+void TLFrame::OnExit(wxCommandEvent& event)
 {
     Close( true );
 }
-void MyFrame::OnAbout(wxCommandEvent& event)
+void TLFrame::OnAbout(wxCommandEvent& event)
 {
     wxMessageBox( "This is a wxWidgets' Hello world sample",
                   "About Hello World", wxOK | wxICON_INFORMATION );
