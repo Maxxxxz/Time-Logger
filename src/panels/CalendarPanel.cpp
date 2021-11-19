21e31afa1caf76433b0e3ae962372374243160f4
Michael Cooper <mc454916@ohio.edu>
2021-11-18 22:31:18 -0500

    Draw calendar lines

--- src/panels/CalendarPanel.cpp
+++ src/panels/CalendarPanel.cpp
@@ -18,5 +18,24 @@ void CalendarPanel::OnPaint(wxPaintEvent& evt)
     // Paint the 6 calendar dividing lines
     // ???
     wxPaintDC dc(this);
-    dc.DrawText(wxString::Format(wxT("%s"), "Hello, calendar!"), 50, 50);
+    // dc.DrawText(wxString::Format(wxT("%s"), "Hello, calendar!"), 50, 50);
+    // Generate line points here
+    
+    const auto LEN = 6;
+
+    wxPoint list[LEN] = {};
+
+    // dc.SetBrush(*wxGREEN_BRUSH); // green filling
+    dc.SetPen(wxPen(wxColor(0,0,0), 1, wxPENSTYLE_DOT));
+
+    wxSize size = this->GetSize();
+
+    for(auto i = 1; i <= LEN; i++)
+    {
+        int x = (size.x*i/7);
+        dc.DrawLine(wxPoint(x, 0), wxPoint(x, size.y));
+    }
+    
+    // dc.DrawRectangle( 300, 100, 400, 200 );
+
 }
