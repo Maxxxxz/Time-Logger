c75e7be3d88ead66ad2c379f8221bbb6e907ad55
Michael Cooper <mc454916@ohio.edu>
2021-11-21 21:10:27 -0500

    Add some test data to a basic list control before implementing DayLogListControl

--- src/panels/CalendarPanel.cpp
+++ src/panels/CalendarPanel.cpp
@@ -4,7 +4,17 @@ CalendarPanel::CalendarPanel(wxFrame* parent) : wxPanel(parent, wxID_ANY, wxDefa
 {
     this->parent = parent;
 
-    Connect(wxEVT_PAINT, wxPaintEventHandler(CalendarPanel::OnPaint));
+    // Connect(wxEVT_PAINT, wxPaintEventHandler(CalendarPanel::OnPaint));
+
+    // this->list = new DayLogListControl();
+    this->list = new wxListCtrl(this);
+
+    // Add some test data...
+
+    this->list->AppendColumn("Test");
+
+    this->list->InsertItem(1, "Testing");
+
     Connect(wxEVT_SIZE, wxSizeEventHandler(CalendarPanel::OnSize));
 }
 
@@ -26,7 +36,7 @@ void CalendarPanel::OnPaint(wxPaintEvent& evt)
     wxPoint list[LEN] = {};
 
     // dc.SetBrush(*wxGREEN_BRUSH); // green filling
-    dc.SetPen(wxPen(wxColor(0,0,0), 1, wxPENSTYLE_DOT));
+    dc.SetPen(wxPen(wxColor(0,0,0), 1, wxPENSTYLE_SOLID));
 
     wxSize size = this->GetSize();
 
@@ -35,6 +45,10 @@ void CalendarPanel::OnPaint(wxPaintEvent& evt)
         int x = (size.x*i/7);
         dc.DrawLine(wxPoint(x, 0), wxPoint(x, size.y));
     }
+
+    int y = size.y/7;
+
+    dc.DrawLine(wxPoint(0, y), wxPoint(size.x, y));
     
     // dc.DrawRectangle( 300, 100, 400, 200 );
 
