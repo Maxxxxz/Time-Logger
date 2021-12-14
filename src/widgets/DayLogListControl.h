61fc480ee9815394878a068a643cd323a5da1f19
Michael Cooper <mc454916@ohio.edu>
2021-12-13 23:14:33 -0500

    Add dayloglist widgets path to cmakelist
    Also, trying to figure out why I can't call the parent functions from the child. Maybe I'm just too tired right now

--- src/widgets/DayLogListControl.h
+++ src/widgets/DayLogListControl.h
@@ -7,10 +7,15 @@
 
 #include "wx/listctrl.h"
 
-class DayLogListControl : public wxListView
+class DayLogListControl : public wxScrolled<wxPanel>
 {
     // Hover event will display an "add" button on the bottom of the list
     // Clicking the "add" button will prompt the add event in which a log is added
+public:
+    DayLogListControl(wxWindow* parent, wxWindowID id = -1, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize, long style = wxHSCROLL|wxVSCROLL, const wxString& name = "scrolledWindow");
+    bool SetBackgroundColour(const wxColour &color);
+private:
+
 };
 
 #endif
